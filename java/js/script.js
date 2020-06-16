c4d442cb8ea0994b730c61ca48123fdc602a7a64
en <vaszko.zsolt@gmail.com>
2020-06-16 14:36:31 +0200

    20200616

---
+++ java/js/script.js
@@ -0,0 +1,15 @@
+console.log("ez egy consol üzenet");
+
+let price1 = 5;
+let price2 = 10;
+let price3 = 50;
+
+
+let total = price1+price2;
+
+let name;
+
+const age = 43; //fssdfdsf
+
+typeof age; // változó tipusának lekérdezése
+
